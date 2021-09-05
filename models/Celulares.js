c8587778f0f31ba1ca8cb5a0bd4528eb2cd2a21f
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-09-05 14:16:52 -0500

    Modelo para celulares

---
+++ models/Celulares.js
@@ -0,0 +1,13 @@
+class Celular {
+  constructor(id, marca, modelo, sistemaOperativo, ram, almacenamiento, color) {
+    this.id = id;
+    this.marca = marca,
+    this.modelo = modelo,
+    this.sistemaOperativo = sistemaOperativo,
+    this.ram = ram,
+    this.almacenamiento = almacenamiento,
+    this.color = color
+  }
+}
+
+module.exports = Celular;
