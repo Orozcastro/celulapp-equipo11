c9acf0725ee29b59d554ad819376c1e28a4f15d9
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-09-05 14:17:25 -0500

    Incluyendo router de celulares

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-var router = require("express").Router();
+var router = require('express').Router();
 
 // Raíz del endpoint
-router.get("/", (req, res) => {
-  res.send("Bienvenido a la API Celulapp");
+router.get('/', (req, res) => {
+  res.send('Bienvenido a la API Celulapp');
 });
 
-router.use("/usuarios", require("./usuarios"));
+router.use('/usuarios', require('./usuarios'));
+
+router.use('/celulares', require('./celulares'));
 
 module.exports = router;
