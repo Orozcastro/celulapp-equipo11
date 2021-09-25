9352762a8569d17e7e58ab042daca39a42d31f1e
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-09-25 12:08:42 -0500

    Agregando variables de entorno

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const mongoose = require('mongoose');
 require('./models/Celulares');
 
 mongoose.connect(
-  'mongodb+srv://equipo11:phoneapi11@cluster0.xpma4.mongodb.net/celulapp?retryWrites=true&w=majority'
+  process.env.MONGO_URI, // obtiene la url de conexión desde las variables de entorno
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
 // debugg
@@ -29,6 +30,8 @@ app.use('/v1', require('./routes'));
 
 // Iniciando el servidor
 const PORT = 4001;
-app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`)
+})
+
+
