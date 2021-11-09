f745ea9be55cc76c20d3d2e896d5bb007c6b945d
L.D. Ramirez Guerra <79238146+DanyGuerra@users.noreply.github.com>
2021-11-09 17:13:08 -0600

    Agregando cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
